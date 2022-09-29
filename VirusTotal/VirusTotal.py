ddefa9c73eedc0116897082a46cfba849fd363ed
Y-Mineoka <114162268+Y-Mineoka@users.noreply.github.com>
2022-09-29 18:43:14 +0900

    Add VirusTotal.py

---
+++ VirusTotal/VirusTotal.py
@@ -0,0 +1,22 @@
+#import json
+import requests
+from requests import RequestException
+
+VIRUS_TOTAL_REQUEST_URL = "https://www.virustotal.com/api/v3/files/"
+
+def serchVirusTotal(hash: str, apikey: str):
+    url = VIRUS_TOTAL_REQUEST_URL + hash
+    try:
+        headers = {
+            "accept": "application/json",
+            "x-apikey": apikey
+        }
+        res = requests.get(url, headers=headers)
+        
+        if res.status_code == 200:
+            return res.json()
+        else:
+            res.raise_for_status()
+    except RequestException as e:
+        print(e.response.text)
+        return

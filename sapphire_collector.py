6621524655f29b5f746c4ee1e56b9afabf3dcfb9
kova-1242 <jokeroki@i.softbank.jp>
2023-09-30 16:44:00 +0900

    commit HybridRequriedData

--- sapphire_collector.py
+++ sapphire_collector.py
@@ -20,6 +20,98 @@ def command_welcome():
 Welcome to SapphireCollector!
 """)
 
+def HybridRequiredData(hybridanalysis_result):
+    data_list = json.loads(hybridanalysis_result)
+    
+    tags = []
+    name= []
+    url=[]
+    score_list=[]
+    vx_family=[]
+    analysis_start_time=[]
+    hosts=[]
+    domains=[]
+    Number_of_matchs = 0
+
+    def maxScore(score_list):
+        max_number = None
+
+        for item in score_list:
+            if isinstance(item, (int, float)):
+                if max_number is None or item > max_number:
+                    max_number = item
+
+        if max_number is not None:
+            return max_number
+        else:
+            return None
+    
+
+    def Delete_the_first_two_characters_in_list(your_list):
+        new_list = [item[2:] for item in your_list]
+        return new_list
+    
+    def Duplicate_removal_and_sorting(your_list):
+        new_list = sorted(list(set(your_list)))
+        return new_list
+    
+    
+    for k in range(len(data_list)):
+        Number_of_matchs = Number_of_matchs + 1
+        data = data_list[k]
+        submiissions = data["submissions"]
+        for i in range(len(submiissions)):
+            target_name= submiissions[i]["filename"]
+            target_url = submiissions[i]["url"]
+            name.append(str(k+1) + ">" +target_name)
+            url.append(str(k+1) + ">" +str(target_url))
+        
+        classificationtags_list = data["classification_tags"]
+        tags_list = data["tags"]
+        for i in range(len(classificationtags_list)):
+            tags.append(str(k+1) + ">"+ classificationtags_list[i])
+        for i in range(len(tags_list)):
+            tags.append(str(k+1) + ">" + tags_list[i])
+
+        score_list.append(data["threat_score"])
+        vx_family.append(str(k+1) +">" +data["vx_family"])
+        analysis_start_time.append(str(k+1) + ">" +data["analysis_start_time"])
+
+        domains_list = data["domains"]
+        for i in range(len(domains_list)):
+            domains.append(str(k+1)+ ">"+ domains_list[i])
+
+        hosts_list = data["hosts"]
+        for i in range(len(hosts_list)):
+            hosts.append(str(k+1) + ">"+ hosts_list[i])
+
+    
+    tags = Duplicate_removal_and_sorting(tags)
+    name = Duplicate_removal_and_sorting(name)
+    url = Duplicate_removal_and_sorting(url)
+    vx_family = Duplicate_removal_and_sorting(vx_family)
+    analysis_start_time = Duplicate_removal_and_sorting(analysis_start_time)
+    domains = Duplicate_removal_and_sorting(domains)
+    hosts = Duplicate_removal_and_sorting(hosts)
+
+    score = maxScore(score_list)
+    
+    
+    OnlyNeedData = {
+        "matchs": Number_of_matchs,
+        "name": name,
+        "tags": tags,
+        "url": url,
+        "score": score,
+        "vx_family": vx_family,
+        "analsis_start_time": analysis_start_time,
+        "domains" : domains,
+        "hosts" : hosts,
+    }
+        
+    result = json.dumps(OnlyNeedData,indent=5)
+    return result
+
 
 def main():
     parser = argparse.ArgumentParser(description="Tool to search and collect malware information from multiple malware database services by hash value")
@@ -46,11 +138,12 @@ def main():
         sys.exit(1)
 
     triage_result = "[[Triage]]\n" + json.dumps(searchTriage(hashType, fileHash, TRIAGE_APIKEY), indent=4)
-    hybridanalysis_result = "[[Hybrid Anarysis]]\n" + json.dumps(searchHybridAnalysis(fileHash, HYBRIDANALYSIS_APIKEY), indent=4)
+    hybridanalysis_result = "[[Hybrid Anarysis]]\n" + HybridRequiredData(json.dumps(searchHybridAnalysis(fileHash, HYBRIDANALYSIS_APIKEY), indent=4))
     virustotal_result = "[[VirusTotal]]\n" + json.dumps(serchVirusTotal(fileHash, VIRUSTOTAL_APIKEY), indent=4)
-
     result_list = [triage_result, hybridanalysis_result, virustotal_result]
     all_result_text = "\n".join(result_list)
+
+
     
     if args.output is None:
         print(all_result_text)

9467136ce2ba2d0f76bbf4c09d7e1007b21dedc4
haraneych <neutralcolor13@gmail.com>
2023-10-03 18:27:23 +0900

    add chatgpt option

--- sapphire_collector.py
+++ sapphire_collector.py
@@ -6,7 +6,8 @@ from util.HashType import HashType
 from Triage.triage import searchTriage
 from HybridAnalysis.hybridanalysis import searchHybridAnalysis,HybridRequiredData
 from VirusTotal.VirusTotal import serchVirusTotal,extract_json,UnixTime_to_Standard_time
-from api_keys import TRIAGE_APIKEY, HYBRIDANALYSIS_APIKEY, VIRUSTOTAL_APIKEY
+from chatgpt.chatgpt import summaryByChatgpt
+from api_keys import TRIAGE_APIKEY, HYBRIDANALYSIS_APIKEY, VIRUSTOTAL_APIKEY, OPENAI_APIKEY
 
 
 def command_welcome():
@@ -21,7 +22,7 @@ Welcome to SapphireCollector!
 """)
 
 
-def output_allresult(hybridanalysis_result_json,triage_result_json,virustotal_result_json):
+def output_allresult(hybridanalysis_result_json,triage_result_json,virustotal_result_json, chatgpt_result):
     print(f"""
 <name>
 VuirsTotal:{virustotal_result_json["name"]}
@@ -48,18 +49,20 @@ VuirsTotal:{virustotal_result_json["c2"]}
 Triage:{triage_result_json["C2ip"][0]["iocs"]["ips"]}
 Hybrid analysis: {hybridanalysis_result_json["hosts"]}
 
-<Summary of malware behavior>
-ここにChatGPTの要約をいれる
-
-
 """)
 
+    if chatgpt_result:
+        print(f"""
+<Summary of malware behavior>
+{chatgpt_result}
 
+""")
 
 
 def main():
     parser = argparse.ArgumentParser(description="Tool to search and collect malware information from multiple malware database services by hash value")
     parser.add_argument("hash", nargs="?", help="Hash value of malware to search for. MD5, SHA1, SHA256 can be used.")
+    parser.add_argument("-c", "--chatgpt", action="store_true", help="Chatgpt explains the behavior of malware.")
     parser.add_argument("-o", "--output", help="File path to output results.")
     args = parser.parse_args()
 
@@ -80,27 +83,32 @@ def main():
     if hashType is None:
         print('Error: Only MDD5, SHA1, SHA256 can be used for hash type.', file=sys.stderr)
         sys.exit(1)
-    
+        
+
     #本番で使う////
     hybridanalysis_result_json = json.loads(HybridRequiredData(json.dumps(searchHybridAnalysis(fileHash, HYBRIDANALYSIS_APIKEY), indent=4)))
-    # triage_result_json = json.loads(json.dumps(searchTriage(hashType, fileHash, TRIAGE_APIKEY), indent=4))
+    triage_result_json = json.loads(json.dumps(searchTriage(hashType, fileHash, TRIAGE_APIKEY), indent=4))
     virustotal_result_json = extract_json(json.loads(json.dumps(serchVirusTotal(fileHash, VIRUSTOTAL_APIKEY), indent=4)))
     #本番ここまで////
-    
+    # description =  [triage_result_json, hybridanalysis_result_json, virustotal_result_json]
+    chatgpt_result = ""
+    if args.chatgpt:
+        chatgpt_result = summaryByChatgpt(OPENAI_APIKEY, description)
     
     #ここから(削除予定)////
-    with open("resultTriage.json","r") as file:
-        triage_result_json = json.load(file)
+    # with open("resultTriage.json","r") as file:
+    #     triage_result_json = json.load(file)
     #ここまで/////
 
 
 
-    if args.output is None:
-        output_allresult(hybridanalysis_result_json,triage_result_json,virustotal_result_json)
-    else:
+
+    if args.output :
         output_filepath = args.output
         with open(output_filepath, "w") as f:
             f.write(output_allresult((hybridanalysis_result_json,triage_result_json,virustotal_result_json)))
+    else:
+        output_allresult(hybridanalysis_result_json,triage_result_json,virustotal_result_json,chatgpt_result)
 
 
 if __name__ == "__main__":

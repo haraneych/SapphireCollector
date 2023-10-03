6e2b6b254ada679b26f5f33ee656febbbc55a84f
kova-1242 <jokeroki@i.softbank.jp>
2023-10-03 21:06:54 +0900

    commit final version

--- sapphire_collector.py
+++ sapphire_collector.py
@@ -7,7 +7,7 @@ from Triage.triage import searchTriage
 from HybridAnalysis.hybridanalysis import searchHybridAnalysis,HybridRequiredData
 from VirusTotal.VirusTotal import serchVirusTotal,extract_json,UnixTime_to_Standard_time
 from chatgpt.chatgpt import summaryByChatgpt
-from api_keys import TRIAGE_APIKEY, HYBRIDANALYSIS_APIKEY, VIRUSTOTAL_APIKEY
+from api_keys import TRIAGE_APIKEY, HYBRIDANALYSIS_APIKEY, VIRUSTOTAL_APIKEY,OPENAI_APIKEY
 
 
 def command_welcome():
@@ -25,50 +25,14 @@ Welcome to SapphireCollector!
 def output_allresult(hybridanalysis_result_json,triage_result_json,virustotal_result_json, chatgpt_result):
     print(f"""
 <name>
-VuirsTotal:{virustotal_result_json["name"]}
-Triage:
-Hybrid analysis: {hybridanalysis_result_json["name"]}
-
-<tags>
-VuirsTotal:{virustotal_result_json["tags"]}
-Triage:{triage_result_json["tag"]}
-Hybrid analysis: {hybridanalysis_result_json["tags"]}
-
-<score>
-VuirsTotal:{virustotal_result_json["malicious"]}/{virustotal_result_json["malicious"]+virustotal_result_json["undetected"]}
-Triage:{triage_result_json["score"]}/10
-Hybrid analysis: {hybridanalysis_result_json["score"]}/100
-
-<analysis start time>
-VuirsTotal:{virustotal_result_json["start_time"]}
-Triage:{triage_result_json["analsys_start_time"]}
-Hybrid analysis: {hybridanalysis_result_json["analsis_start_time"]}
-
-<Suspected IP Address of C2 Server>
-VuirsTotal:{virustotal_result_json["c2"]}
-Triage:{triage_result_json["C2ip"][0]["iocs"]["ips"]}
-Hybrid analysis: {hybridanalysis_result_json["hosts"]}
-
-""")
-
-    if chatgpt_result:
-        print(f"""
-<Summary of malware behavior>
-{chatgpt_result}
-
-""")
-
-def result_format(hybridanalysis_result_json,triage_result_json,virustotal_result_json):
-    print(f"""
-<name>
 VuirsTotal:{json.dumps(virustotal_result_json["name"],indent=2)}
 Triage:
 Hybrid analysis: {json.dumps(hybridanalysis_result_json["name"],indent=2)}
 
 <tags>
-VuirsTotal:{virustotal_result_json["tags"]}
-Triage:{triage_result_json["tag"]}
-Hybrid analysis: {hybridanalysis_result_json["tags"]}
+VuirsTotal:{json.dumps(virustotal_result_json["tags"],indent=2)}
+Triage:{json.dumps(triage_result_json["tag"],indent=2)}
+Hybrid analysis: {json.dumps(hybridanalysis_result_json["tags"],indent=2)}
 
 <score>
 VuirsTotal:{virustotal_result_json["malicious"]}/100
@@ -82,14 +46,18 @@ Hybrid analysis: {hybridanalysis_result_json["analsis_start_time"]}
 
 <Suspected IP Address of C2 Server>
 VuirsTotal:{json.dumps(virustotal_result_json["c2"],indent=2)}
-Triage:
+Triage:{json.dumps(triage_result_json["C2ip"],indent=2)}
 Hybrid analysis: {json.dumps(hybridanalysis_result_json["hosts"],indent=2)}
 
-<Summary of malware behavior>
-ここにChatGPTの要約をいれる
+""")
 
+    if chatgpt_result:
+        print(f"""
+<Summary of malware behavior>
+{chatgpt_result}
 
 """)
+
 
 
 def main():
@@ -119,20 +87,15 @@ def main():
 
         
 
-    #本番で使う////
     hybridanalysis_result_json = json.loads(HybridRequiredData(json.dumps(searchHybridAnalysis(fileHash, HYBRIDANALYSIS_APIKEY), indent=4)))
     triage_result_json = json.loads(json.dumps(searchTriage(hashType, fileHash, TRIAGE_APIKEY), indent=4))
     virustotal_result_json = extract_json(json.loads(json.dumps(serchVirusTotal(fileHash, VIRUSTOTAL_APIKEY), indent=4)))
-    #本番ここまで////
-    # description =  [triage_result_json, hybridanalysis_result_json, virustotal_result_json]
+
+    description =  [triage_result_json, hybridanalysis_result_json, virustotal_result_json]
     chatgpt_result = ""
     if args.chatgpt:
         chatgpt_result = summaryByChatgpt(OPENAI_APIKEY, description)
     
-    #ここから(削除予定)////
-    # with open("resultTriage.json","r") as file:
-    #     triage_result_json = json.load(file)
-    #ここまで/////
 
 
 
@@ -141,8 +104,10 @@ def main():
         output_filepath = args.output
         with open(output_filepath, "w") as f:
             f.write(output_allresult((hybridanalysis_result_json,triage_result_json,virustotal_result_json)))
+        
     else:
         output_allresult(hybridanalysis_result_json,triage_result_json,virustotal_result_json,chatgpt_result)
+
 
 if __name__ == "__main__":
     main()
